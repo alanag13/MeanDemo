a4c4b3f98060acc73edaa52870f2faba7ae2a3b9
alanag13 <alan.grgic@gmail.com>
2015-07-15 23:47:42 -0500

    boilerplate for 'hello world' express/node app

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var port = process.env.PORT || 80;
+
+//configure some server information: views and caching
+app.set('views', __dirname + '/client/views');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/client/_resources', { maxAge: 2592000000 }));
+
+//route all requests to the index page
+app.get('*', function(req, res){
+	res.render('index');
+});
+
+//start the server and listen on port 80
+http.listen(port, function () {
+    console.log('Listening on port ' + port);
+});
